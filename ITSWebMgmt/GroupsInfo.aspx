325f1a479297851eea89f8e6ea3337470652b45e
yrke <kenneth@yrke.dk>
2015-12-11 09:51:53 +0100

    added groupsinfo

---
+++ ITSWebMgmt/GroupsInfo.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GroupsInfo.aspx.cs" Inherits="ITSWebMgmt.GroupsInfo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <!-- Loader -->
+    <div id="loader">
+        <div class="ui active dimmer" style="display: none">
+            <div class="ui text loader">Loading</div>
+        </div>
+    </div>
+
+    <!-- page seach header -->
+    <div>
+        <h1>Group Info</h1>
+        <!--
+        Group Name
+        <div class="ui action input">
+            <asp:TextBox ID="txtbxGroupName" runat="server" Text="" CssClass="ui input focus" placeholder="Search..." />
+            <asp:Button runat="server" CssClass="ui button" ID="sumbit" OnClick="sumbit_Click" Text="Søg" OnClientClick='$("#loader > div").show("fast");' />
+        </div>
+        -->
+    </div>  
+
+
+
+    <!-- page content -->
+
+
+    <script>
+        $(document).ready(function () {
+            $('.menu .item').tab({ history: false });
+        });
+    </script>
+
+    <br />
+    <br />
+    <div runat="server" id="ResultDiv">
+
+
+
+
+        <div class="ui grid">
+            <div class="four wide column">
+                <div class="ui vertical fluid tabular menu">
+                    <!--<a class="active item" data-tab="basicinfo">Basic Info</a>
+                    <a class="item" data-tab="members">Members</a>-->
+                    <!--<a class="item" data-tab="">Recurs memeberof?</a>-->
+                    <a class="active item" data-tab="rawData">Raw Data</a>
+
+                </div>
+            </div>
+            <div class="twelve wide stretched column">
+                <div class="ui tab segment">
+                    none<!-- spacer as the fist elemen else is placed differencet -->
+                </div>
+    <!--            <div class="ui active tab segment" data-tab="basicinfo">
+                </div>
+                <div class="ui tab segment" data-tab="members">
+                    advanced
+                </div>
+                <div class="ui tab segment" data-tab="memberOf">
+                    advanced
+                </div>
+        -->     <div class="ui active tab segment" data-tab="rawData">
+                    <h2>Raw Data</h2>
+                    <asp:Label runat="server" ID="labelRawData" />
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+</asp:Content>
---
+++ ITSWebMgmt/GroupsInfo.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITSWebMgmt
+{
+    public partial class GroupsInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResultDiv.Visible = false;
+
+
+            if (!IsPostBack)
+            {
+                //GroupPath the AD path to an object
+                //GroupName the (domina) + group name (now we need to search for the gorup)
+                //Default user an AD path to the user to be default to add/remove from a group
+                //Action (action to preform)
+                //Return to (only used on action to) page to return to after action
+                String groupPath = Request.QueryString["grouppath"];
+                String groupName = Request.QueryString["groupname"];
+                String defaultUser = Request.QueryString["defaultuser"];
+                if (groupPath != null)
+                {
+                    //Handle a group path
+                    var groupDE = convertADpathToDirectoryEntry(groupPath);
+                    if (groupDE != null) { 
+                        buildResult(groupDE);
+                    }
+                }
+            }
+            else //Is postback
+            {
+
+            }       
+
+
+
+        }
+
+        protected void sumbit_Click(object sender, EventArgs e)
+        {
+            //Search button i pressed, do stuff
+        }
+
+        protected DirectoryEntry convertADpathToDirectoryEntry(string adpath)
+        {
+            ///XXX we expect a group check its a group
+            var groupDE = new DirectoryEntry(adpath);
+            if (groupDE.SchemaEntry.Name.Equals("group", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return groupDE;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected void buildResult(DirectoryEntry group)
+        {
+            buildBasicInfo(group);
+            buildMembers(group);
+            buildMemberOf(group);
+            buildRaw(group);
+
+            ResultDiv.Visible = true;
+
+        }
+
+        private void buildRaw(DirectoryEntry group)
+        {
+            var generator = new RawADGridGenerator();
+            var result = generator.buildRawSegment(group);
+
+            labelRawData.Text = result.ToString();
+        
+        }
+
+        private void buildMemberOf(DirectoryEntry group)
+        {
+            return;
+        }
+
+        private void buildMembers(DirectoryEntry group)
+        {
+            return;
+        }
+
+        private void buildBasicInfo(DirectoryEntry group)
+        {
+            return;
+        }
+
+
+    }
+}
